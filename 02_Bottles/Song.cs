9b57d3173e91dbc7f01a736fd17ee2a5c07dbc36
MathiasBellerbySylve <MathiasBellerbySylve@DESKTOP-6M2QC1B>
2016-09-08 10:34:58 +0200

    Start off Assignment

---
+++ 02_Bottles/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Bottles
+{
+    class Song
+    {
+        public static CountBottles(int NumberOfBottles)
+        {
+            switch (NumberOfBottles)
+            {
+                case 1:
+                    return;
+                    break;
+            }
+        }
+    }
+}
